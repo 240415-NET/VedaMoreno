08ca7d5ff81426fa15c3cd82ed8fdf80448cdb0d
Moreno <veda.moreno@yahoo.com>
2024-04-23 17:49:03 -0400

    String Array Class Notes

---
+++ WEEK_2/StringArray/StringArray-Class-Notes.cs
@@ -0,0 +1,29 @@
+using System;
+					
+namespace StringArray;
+
+class Program
+{
+    static void Main(string[] args)
+    {  
+            
+            int student_one_grade = 97;
+            int student_two_grade = 93;
+            int student_three_grade = 100;
+            int student_four_grade = 85;
+    
+           
+            Console.WriteLine(student_one_grade);
+            Console.WriteLine(student_two_grade);
+            Console.WriteLine(student_three_grade);
+            Console.WriteLine(student_four_grade);
+
+    }
+
+
+}
+
+
+     
+
+
