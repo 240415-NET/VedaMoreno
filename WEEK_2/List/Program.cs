f665a747dc344341ddf61e2a5f6a90c744610eb5
Moreno <veda.moreno@yahoo.com>
2024-04-26 17:40:39 -0400

    List method

---
+++ WEEK_2/List/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace List;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        List<string> storeList = new List<string>();
+
+        storeList.Add("Meijer");
+        storeList.Add("Kroger");
+        storeList.Add("Publix");
+        storeList.Add("Whole Foods");
+        storeList.Add("Fresh Market");
+        storeList.Add("Sprouts");
+        storeList.Add("Winn Dixie");
+
+        foreach(string store in storeList)
+
+        { 
+
+            Console.WriteLine(store);
+
+        }
+
+    }
+}
