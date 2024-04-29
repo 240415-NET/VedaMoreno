b7e38e09aec417a06aa25a6d53bdc621f3b69b70
Moreno <veda.moreno@yahoo.com>
2024-04-29 16:34:58 -0400

    If-Else Flows

--- Week_3/Test-Study/Program.cs
+++ Week_3/Test-Study/Program.cs
@@ -3,29 +3,59 @@
 class Program
 {
     static void Main(string[] args)
+
+    //if - else if statement
+    //     {
+    //         int i = 20; 
+    //         int j = 20; 
+
+    //         if (i == j)
+    //         {
+
+    //             Console.WriteLine("i is equal to j");
+
+    //         }
+    //         else if (i>j)
+    //         {
+
+    //             Console.WriteLine("i is greater than j");
+
+
+    //         }
+    //         else if(i<j)
+    //         {
+
+    //             Console.WriteLine("i is less than j");
+    //         }
+
+
+    //     }
+    // }
+
+
+    // Nested if else statement 
+
     {
-        int i = 10; 
-        int j = 20; 
+        int i = 10;
+        int j = 20;
 
-        if (i == j)
+        if (i != j)
         {
-
-            Console.WriteLine("i is equal to j");
+            if (i < j)
+            {
+                Console.WriteLine("i is less than j");
+            }
+            else if (i > j)
+            {
+                Console.WriteLine("is greater than j");
+            }
+            else
+                Console.WriteLine("i is equal to j");
 
         }
-        else if (i>j)
-        {
 
-            Console.WriteLine("i is greater than j");
-
-    
-        }
-        else if(i<j)
-        {
-
-            Console.WriteLine("i is less than j");
-        }
-
-       
     }
 }
+
+
+
