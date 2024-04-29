c4cdc96d8b6939f6554a7648c6cb83ccdc1c02e2
Moreno <veda.moreno@yahoo.com>
2024-04-29 17:37:30 -0400

    For Loop

---
+++ Week_3/Test-For Loops/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Test_For_Loops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Example of for loop
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Value of i: {0}", i);
+        }
+
+
+
+        // Example of REVERSE for loop
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Value of i: {0}", i);
+        }
+    }
+
+}
+
