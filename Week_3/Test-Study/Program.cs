8af893c651ec27e79064587eabb783223799ae85
Moreno <veda.moreno@yahoo.com>
2024-04-29 16:05:11 -0400

    If Else Flow

---
+++ Week_3/Test-Study/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Test_Study;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 10; 
+        int j = 20; 
+
+        if (i == j)
+        {
+
+            Console.WriteLine("i is equal to j");
+
+        }
+        else if (i>j)
+        {
+
+            Console.WriteLine("i is greater than j");
+
+    
+        }
+        else if(i<j)
+        {
+
+            Console.WriteLine("i is less than j");
+        }
+
+       
+    }
+}
