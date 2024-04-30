b7cfb96f68f5dd0d99deb7d8787f8520f41ead8c
Moreno <veda.moreno@yahoo.com>
2024-04-29 22:48:51 -0400

    Switch Statement

---
+++ Week_3/Switch/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Switch;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int g = 30;
+
+        switch (g)
+        {
+            case 10: 
+                Console.WriteLine("Value of g is 10");
+                break;
+            case 20:
+                Console.WriteLine("Value of g is 20");
+                break;
+            case 30:  
+                Console.WriteLine("Value of g is 30");
+                break; 
+            default:
+                Console.WriteLine("Value of g is unkown");
+                break;
+                
+        }
+
+    }
+}
