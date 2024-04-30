3cc7061d93c1c0c745e8e7883ba1b45c7bc16785
Moreno <veda.moreno@yahoo.com>
2024-04-29 21:44:51 -0400

    For-While-Do While Loops

--- Week_3/Test-For Loops/Program.cs
+++ Week_3/Test-For Loops/Program.cs
@@ -10,14 +10,45 @@ class Program
             Console.WriteLine("Value of i: {0}", i);
         }
 
-
-
-        // Example of REVERSE for loop
-        for (int i = 0; i < 10; i++)
+        // // Example of REVERSE for loop
+        for (int i = 10; i > 0; i--)
         {
             Console.WriteLine("Value of i: {0}", i);
         }
+
+        // Example of While Loop and using break to exit
+
+        int k = 0;
+
+        while (true)
+        {
+            Console.WriteLine("k = {0}", k);
+            k++;
+
+            if (k > 7)
+                break;
+        }
+
+        // Example of DO While Loop 
+        // Use break or return to exit from the do while loop
+
+        int j = 0;
+
+        do
+        {
+            Console.WriteLine("j = {0}", j);
+            j++;
+
+            if (j > 4)
+                break;
+
+        } while (j < 8);
+
     }
 
 }
+
+
+
+
 
