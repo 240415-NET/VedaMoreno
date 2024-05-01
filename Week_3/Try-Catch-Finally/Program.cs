175399145044aaa09124e50e0510555b47850fc0
Moreno <veda.moreno@yahoo.com>
2024-04-30 22:14:42 -0400

    Try-Catch-Finally Practice

--- Week_3/Try-Catch-Finally/Program.cs
+++ Week_3/Try-Catch-Finally/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Try_Catch_Finally;
+﻿using System;
+
+namespace Try_Catch_Finally;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        try
+        {
+            Console.WriteLine("Please input a number");
+
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Square root of {num} is {num * num}");
+        }
+
+        catch
+        {
+            Console.Clear();
+            Console.WriteLine("Format error occured, please check number");
+        }
     }
 }
