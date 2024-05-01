056ac165d65d52644d02c5d1ddb0b46573f9d2b9
Moreno <veda.moreno@yahoo.com>
2024-05-01 00:19:40 -0400

    Array Practice

---
+++ Week_3/Arrays/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+
+namespace Arrays;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       string[] city = new string[3]{"Tampa", "Jacksonville", "Miami"};
+       Console.WriteLine(city[1]); 
+       Console.WriteLine(city[0]);
+    }
+}
