ab434f9e27cb793b62dc5af69396135fb899d823
Moreno <veda.moreno@yahoo.com>
2024-04-30 23:02:05 -0400

    List Practice

--- WEEK_2/List/Program.cs
+++ WEEK_2/List/Program.cs
@@ -1,9 +1,11 @@
-﻿namespace List;
+﻿using System;
 
-class Program
+namespace List;
+
+public class Program
 {
     static void Main(string[] args)
-    {   
+    {
         List<string> storeList = new List<string>();
 
         storeList.Add("Meijer");
@@ -14,13 +16,35 @@ class Program
         storeList.Add("Sprouts");
         storeList.Add("Winn Dixie");
 
-        foreach(string store in storeList)
+        foreach (string store in storeList)
 
-        { 
+        {
 
             Console.WriteLine(store);
 
         }
 
+
+        List<string> pastryList = new List<string>();
+
+        pastryList.Add("cookie");
+        pastryList.Add("donut");
+        pastryList.Add("cake");
+        pastryList.Add("canoli");
+        pastryList.Add("pie");
+
+        foreach (string pastry in pastryList)
+
+        {
+            Console.WriteLine(pastry);
+        }
     }
 }
+
+
+
+
+
+
+
+
