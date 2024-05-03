d5c71f79d46c7e49ddf11f27876bdf4c46dbd0b2
Moreno <veda.moreno@yahoo.com>
2024-05-03 01:00:01 -0400

    Store-Grocery List

---
+++ Market/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Market;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string itemsInList = "";
+        bool enterAdditionalItem = true;
+        string answer = "";
+        List<string> storeList = new List<string>();
+
+        Console.WriteLine("Store items Menu");
+
+        do
+        {
+            Console.WriteLine("Please enter grocery item: ");
+            itemsInList = Console.ReadLine();
+
+            storeList.Add(itemsInList);
+
+            Console.WriteLine("Do you want to enter another item, Yes or No?");
+            answer = Console.ReadLine();
+
+
+            if (answer == "yes")
+            {
+                enterAdditionalItem = true;
+
+            }
+
+            else if (answer == "no")
+            {
+
+                enterAdditionalItem = false;
+
+            }
+
+
+        } while (enterAdditionalItem == true);
+
+        Console.WriteLine("List:  " + storeList[0]);
+
+    }
+}
